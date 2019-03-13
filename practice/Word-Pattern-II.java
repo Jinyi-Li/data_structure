119dee38867dd3ce599953240e864f09c1a4517f
Jinyi-Li <li.jinyi@outlook.com>
2019-03-12 23:54:03 -0400

    [add] use dfs to check word patterns

---
+++ practice/Word-Pattern-II.java
@@ -0,0 +1,74 @@
+public class Solution {
+    /**
+     * @param pattern: a string,denote pattern string
+     * @param str: a string, denote matching string
+     * @return: a boolean
+     */
+    public boolean wordPatternMatch(String pattern, String str) {
+        // write your code here
+        
+        Map<Character, String> patternWordMap = new HashMap<>();
+        Set<String> usedWords = new HashSet<>();
+        
+        //  r edblue r edblue
+        //  a b     a    b
+        
+        return match(pattern, str, patternWordMap, usedWords);
+    }
+    
+    private boolean match(String pattern, String str, 
+        Map<Character, String> patternWordMap, Set<String> usedWords){
+        
+        if(pattern.length() == 0){
+            if(str.length() == 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        
+        char firstPattern = pattern.charAt(0);
+        // check if word matches pattern-word mapping
+        if(patternWordMap.containsKey(firstPattern)){
+            
+            String mappedWord = patternWordMap.get(firstPattern);
+            int len = mappedWord.length();
+            
+            // no, it doesn't match
+            if(str.length() < len || !str.substring(0, len).equals(mappedWord)){
+                return false;
+            }
+            // yes, it matches. continue checking rest of str.    
+            return match(pattern.substring(1), str.substring(len), patternWordMap, usedWords);
+        }
+        
+        // no such mapping info yet. try different mappings.
+        for(int i = 0; i < str.length(); i++){
+            String newWord = str.substring(0, i + 1);
+            
+            // this word is already mapped to a pattern, don't use it.
+            if(usedWords.contains(newWord)){
+                continue;
+            }
+            
+            // add a new mapping relation.
+            patternWordMap.put(firstPattern, newWord);
+            usedWords.add(newWord);
+            
+            // if this works till the end, return true.
+            if(match(pattern.substring(1), str.substring(newWord.length()), 
+                patternWordMap, usedWords)){
+                
+                return true;
+            }
+            
+            // otherwise, try another mapping.
+            patternWordMap.remove(firstPattern);
+            usedWords.remove(newWord);
+        }
+        
+        // after tried all possible mapping, still not working, return false.
+        return false;
+    }
+    
+}
